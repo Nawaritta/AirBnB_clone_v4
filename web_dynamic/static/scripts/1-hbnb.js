118f6aa6f79e97911b182013b7bb686d40853874
Nawaritta <Nor.benhaddou@gmail.com>
2023-11-03 20:49:41 +0000

    task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    let checked = {};
+    $('input[type="checkbox"]').on('change', function() {
+	const amenityID = $(this).data('id');
+	$(this).is(':checked') ? checked[amenityID] = true : delete checked[amenityID];
+	$('.amenities h4').text(Object.keys(checked).join(', '));
+    });
+});
